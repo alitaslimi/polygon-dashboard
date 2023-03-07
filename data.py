3863d86f67af81d58864205c9de387dea6d0c9e1
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-03-07 15:06:31 +0330

    Add files via upload

--- data.py
+++ data.py
@@ -3,10 +3,16 @@ import streamlit as st
 import pandas as pd
 
 # Data Sources
+collections_csv = pd.read_csv('collections.csv')
+collection_dict = pd.Series(collections_csv.Name.values,index=collections_csv.Collection).to_dict()
+
 # @st.cache(ttl=1000, allow_output_mutation=True)
 def get_data(query):
     if query == 'Mints Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/f17f19c1-7545-4716-b366-780827deaebb/data/latest')
+    
+    if query == 'Mints Collections':
+        return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/07761e94-4c1f-462c-a646-2be49ffb4fdb/data/latest')
     
     elif query == 'Mints Weekly':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/b781bb55-3bfd-40d4-80ec-138e07710549/data/latest')
@@ -34,6 +40,11 @@ def get_data(query):
     
     elif query == 'Washed Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/a5044912-5248-4b80-862a-e8fba60e61e2/data/latest')
+    
+    elif query == 'Washed Collections':
+        df = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/e9192f19-4c3f-46b5-ac76-10c4496b6def/data/latest')
+        df["Collection"] = df["Collection"].map(collection_dict)
+        return df
     
     elif query == 'Washed Weekly':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/5a52f1a9-e121-4967-b0df-cbc35d8a5c13/data/latest')
